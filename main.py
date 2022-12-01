f787c04450aa7e07810b0a7536de4dc7b908d858
Armanchev Pavel <shoker_84@bk.ru>
2022-12-02 09:33:16 +1000

    initiation

---
+++ main.py
@@ -0,0 +1,9 @@
+import handlers
+from database.models import DataBaseModel
+from loader import bot, logger
+
+
+if __name__ == '__main__':
+    DataBaseModel._init_user_tables()
+    bot.infinity_polling()
+    logger.info('bot start working')
